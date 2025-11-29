172b3a345fa7376fd9f37ce9daa4150d51bc0d05
fidok15 <175498067+fidok15@users.noreply.github.com>
2025-11-30 00:21:18 +0100

    feat: added buffer

--- model.py
+++ model.py
@@ -4,16 +4,18 @@ import torch.nn.functional as F
 class DQNmodel(nn.Module):
     def __init__(self, input_shape, num_stats, num_actions):
         super(DQNmodel, self).__init__()
-
+        #parametry przekazywanego swiata 
         channel, height, width = input_shape
         
         self.conv1 = nn.Conv2d(channel,32,3,padding=1)
         self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
 
+        #wyplaszczenie naszego grida swiata
         flatten_dim = 64 * height * width 
         self.fc_conv = nn.Linear(flatten_dim, 128)
+        #statysyki
         self.fc_stats = nn.Linear(num_stats, 64)
-        
+        #polaczenie swiata i statystyk
         self.fc_combined = nn.Linear(128 + 64, 128)
         self.output = nn.Linear(128, num_actions)
     
